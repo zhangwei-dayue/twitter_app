ce7950c2698325e881ec9a86a1c8b7442e502a18
zhangwei-dayue <zhangwei19890518@gmail.com>
2017-08-15 22:55:07 +0800

    增加显示关注用户的微博

--- config/application.rb
+++ config/application.rb
@@ -1,6 +1,7 @@
 require_relative 'boot'
 
 require 'rails/all'
+require File.expand_path('../boot', __FILE__)
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -11,5 +12,6 @@ module TwitterApp
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+    config.action_view.embed_authenticity_token_in_remote_forms = true
   end
 end
